b414f3c58330f65db89c252fad9bac64d5aae7c1
igorvieira09 <iv4761641@gmail.com>
2025-03-11 12:06:38 -0300

    Update Projeto super trunfo.c

--- Projeto super trunfo.c
+++ Projeto super trunfo.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 
-int main(){
+  int main(){
 
+    char estado[10];
+    char codigo[10];
+    char Cidade[10];
+    int populaçao;
+    float Àrea;
+    float pib;
+    int pontos;
+     
+     printf("Digite o nome do estado: \n");
+      scanf("%s",&estado);
+      
+      printf("Digite o Codigo da carta: \n");
+       scanf("%s",&codigo);
+        
+       printf("Digite o Nome da Cidade:: \n]");
+        scanf("%s",&Cidade);
+         
+        printf("Digite o Numero da Populaçao: \n ");
+         scanf("%d",&populaçao);
+          
+         printf("Digite a Àrea em km²: \n");
+          scanf("%f",&Àrea);
+
+          printf("Digite o PIB Da Cidade: \n");
+           scanf("%f",&pib);
+
+           printf("Digite o Numero de Pontos turisticos: \n");
+            scanf("%d",&pontos);
+
+
+
+
+            
+            printf("Digite o nome do estado: \n");
+            scanf("%s",&estado);
+            
+            printf("Digite o Codigo da carta: \n");
+             scanf("%s",&codigo);
+              
+             printf("Digite o Nome da Cidade:: \n]");
+              scanf("%s",&Cidade);
+               
+              printf("Digite o Numero da Populaçao:\n  ");
+               scanf("%d",&populaçao);
+                
+               printf("Digite a Àrea em km²: \n");
+                scanf("%f",&Àrea);
+      
+                printf("Digite o PIB Da Cidade: \n");
+                 scanf("%f",&pib);
+      
+                 printf("Digite o Numero de Pontos turisticos: \n");
+                  scanf("%d",&pontos);
+
+
+
+
+return 0;
 
 
 
